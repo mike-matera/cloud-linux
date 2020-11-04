bcdec8a23772f24c750dd71cbdefb949080aef9a
Mike Matera <maximus@fatboycentral.com>
2020-11-04 10:04:53 -0800

    Ready?

--- cis-90/tests/midterm-2-fa20/midterm-2-fa20.py
+++ cis-90/tests/midterm-2-fa20/midterm-2-fa20.py
@@ -24,24 +24,35 @@ import pathlib
 
 from lifealgorithmic.linux.test import LinuxTest
 
-test = LinuxTest("Ohhh my gooooshh asdfasdf")
+test = LinuxTest("Day after election day nerves and bummer.")
 
 start_files = [    
-    ['cat', '644', 'felis sylvesteris (animal)'],
-    ['dog', '644', 'xxx (animal)'],
-    ['lizard', '644', 'xxx (animal)'],
-    ['pitosporum', '644', 'xxx (plant)'],
+    ['Hawaii', '644', 'Island in the Pacific ocean'],
+    ['Samoa', '644', 'Island in the Pacific ocean'],
+    ['Kiribati', '644', 'Island in the Pacific ocean'],
+    ['Ireland', '644', 'Island in the Atlantic ocean'],
+    ['Madeira', '644', 'Island in the Atlantic ocean'],
+    ['Azores', '644', 'Island in the Atlantic ocean'],
+    ['Langkawi', '644', 'Island in the Indian ocean'],
+    ['Sabang', '644', 'Island in the Indian ocean'],
+    ['Nublar', '644', 'Island in Fiction'],
+    ['Hydra', '644', 'Island in Fiction'],
 ]
 
 end_files = [
-    ['animals/cat', '644', 'felis sylvesteris (animal)'],
-    ['animals/dog', '644', 'xxx (animal)'],
-    ['animals/lizard', '644', 'xxx (animal)'],
-    ['plants/pitosporum', '644', 'xxx (plant)'],
+    ['Pacific/Hawaii',   '640', 'Island in the Pacific ocean'],
+    ['Pacific/Samoa',    '640', 'Island in the Pacific ocean'],
+    ['Pacific/Kiribati', '640', 'Island in the Pacific ocean'],
+    ['Atlantic/Ireland', '600', 'Island in the Atlantic ocean'],
+    ['Atlantic/Madeira', '600', 'Island in the Atlantic ocean'],
+    ['Atlantic/Azores',  '600', 'Island in the Atlantic ocean'],
+    ['Indian/Langkawi',  '400', 'Island in the Indian ocean'],
+    ['Indian/Sabang',    '400', 'Island in the Indian ocean'],
+    ['Fiction/Nublar',   '440', 'Island in Fiction'],
+    ['Fiction/Hydra',    '440', 'Island in Fiction'],
 ]
 
-
-def setup_files(files, startdir=pathlib.Path("Organisms")):
+def setup_files(files, startdir=pathlib.Path("Files")):
     subprocess.run(f"rm -rf {startdir}", shell=True)
     subprocess.run(f"mkdir {startdir}", shell=True)
     for path, mode, contents in files:
@@ -50,7 +61,7 @@ def setup_files(files, startdir=pathlib.Path("Organisms")):
         subprocess.run(f"chmod {mode} {startdir / path}", shell=True)
 
 
-def check_files(files, startdir=pathlib.Path("Output")):
+def check_files(files, startdir=pathlib.Path("Oceans")):
     for path, mode, contents in files:
         with open(startdir / path) as fh:
             assert contents == fh.read(), f"The contents of {path} don't match."
@@ -60,31 +71,67 @@ def check_files(files, startdir=pathlib.Path("Output")):
 
 
 @test.question(20)
-def files():
+def make_files():
     """
-    Create the following files:
-    foo 
-    bar 
-    bak
-    """
-    check_files(end_files)
+    I have created a directory called "Files" in the current direcory. 
+    Inside of "Files" you will see 10 files named after islands. Each island 
+    file contains the name of the ocean it is in. Reorganize the files so 
+    that they are in directories named after their oceans. The resulting 
+    directory structure should look like this: 
 
+      Oceans/
+        Atlantic/
+        Pacific/
+        ... 
+
+    The "Oceans" directory should be in the current directory. Set the 
+    permissions on the island files as described below:
+
+      1. No islands should be readable or writable by others
+      2. Pacific islands should be r/w for the user and read only for the group
+      3. Atlantic islands should r/w for the user an no group access 
+      4. Indian islands should read only for the user 
+      5. Fictional islands should be read only for the user and group.
+
+    """
+    setup_files(start_files)
+    input("Check for the files and press Enter.")
+
+@test.question(10)
+def test_pacific():
+    """I'm about to check the Pacific islands."""
+    check_files(end_files[0:3])
+
+@test.question(10)
+def test_atlantic():
+    """I'm about to check the Atlantic islands."""
+    check_files(end_files[3:6])
+
+@test.question(10)
+def test_indian():
+    """I'm about to check the Indian islands."""
+    check_files(end_files[6:8])
+
+@test.question(10)
+def test_fictional():
+    """I'm about to check the Fictional islands."""
+    check_files(end_files[8:])
 
 @test.question(10)
 def deep_file():
     """
-    I just created a directory called "deep" in your home directory. Explore it until
-    you get to the deepest subdirectory in it. Inside the deepest subdirectory you will 
-    find a secret file. What are the contents of the file? 
+    I just created a directory called "deep" in the "Files" directory. Explore 
+    it until you get to the deepest subdirectory in it. Inside the deepest 
+    subdirectory you will find a secret file. What are the contents of the file? 
     """
     message = 'fliberdyjibit'
-    path = pathlib.Path("deep/there's/a/light/over/at/the/Frankenstein/place")
+    path = pathlib.Path("Files/deep/there's/a/light/over/at/the/Frankenstein/place")
     file = path / '.secret'
     subprocess.run(['mkdir', '-p', path])
     with open(file, 'w') as fh:
-        fh.write(message)
+        fh.write(message + "\n")
     
-    got = input("What's the secret message?")
+    got = input("What's the secret message? ").strip()
     assert got == message, "That's not the message."
 
 
@@ -100,8 +147,10 @@ def this_process():
 @test.question(10)
 def fork_kill():
     """
-    This program just started another process. Find the child process and kill it with signal 9. 
+    When you hit Enter the test will launch a child process. Find the child 
+    process and kill it with signal number 9. 
     """
+    input("Hit enter to create the child process. ")
     pid = os.fork()
     if pid == 0:
         os.execlp('tail', 'tail', '-n', '0', '-f', '/etc/passwd')
@@ -109,16 +158,17 @@ def fork_kill():
     else:
         print(f"Waiting for you to kill the child process.")
         ecode = os.waitpid(pid, 0)
-        assert ecode[1] == 9, """The program wasn't killed right.""" 
+        assert ecode[1] == 9, """The program wasn't killed with signal 9.""" 
 
 
 @test.question(10)
 def count_files():
     """
-    Count all of the file in the /usr directory. 
+    Count all of the regular files (not links or directories) under the /usr
+    directory, including all of its subdirectories.  
     """
     got = int(input("How many files are there? "))
-    find = subprocess.run('find /usr | wc -l', encoding='utf-8', shell=True, stdout=subprocess.PIPE)
+    find = subprocess.run('find /usr -type f | wc -l', encoding='utf-8', shell=True, stdout=subprocess.PIPE)
     files = int(find.stdout.strip())
     assert got == files, "That's not the right number of files."
 
@@ -129,11 +179,9 @@ def midterm_fa2020() :
     '''
 
     print(midterm_fa2020.__doc__)
-    setup_files(start_files)
-    test.run(debug=False, only=["count_files"])
+    test.run(debug=False)
     print("All done!\n\n")    
 
-# Mike Gleason
 
 if __name__ == '__main__' :
     midterm_fa2020()
