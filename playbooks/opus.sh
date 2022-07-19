1783294fabd8581e27134df39eab204dc1744db5
Mike Matera <maximus@fatboycentral.com>
2022-07-19 15:21:02 -0700

    Install labs on opus.

--- playbooks/opus.sh
+++ playbooks/opus.sh
@@ -1,6 +1,7 @@
 #! /bin/bash 
 
 ansible-playbook --connection=local --inventory=127.0.0.1, --limit 127.0.0.1 base.yaml 
+ansible-playbook --connection=local --inventory=127.0.0.1, --limit 127.0.0.1 labs.yaml 
 
 python3 ./generate-users.py cis90 29308 > users-data.yaml 
 ansible-playbook --connection=local --inventory=127.0.0.1, --limit 127.0.0.1 users.yaml 
