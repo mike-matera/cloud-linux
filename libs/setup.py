49706b21e13463a10d8447e3b869b0e5e8abd7c5
Mike Matera <matera@lifealgorithmic.com>
2022-09-07 21:39:50 +0000

    Fix.

--- libs/setup.py
+++ libs/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     install_requires=[
         'canvasapi',
-        'nacl',
+        'pynacl',
         'pyyaml',
         'psutil',
         'importlib-metadata; python_version == "3.10"',
