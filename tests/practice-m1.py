07d7f8475dafc519495a0ae4588b0094a26323d9
Mike Matera <matera@lifealgorithmic.com>
2022-09-29 04:21:58 +0000

    Overhaul practice 1

--- tests/practice-m1.py
+++ tests/practice-m1.py
@@ -1,20 +1,19 @@
-import platform
-import subprocess
-import psutil 
 import os
 import stat
+import psutil 
 import pathlib
-from random import random 
-from re import sub
+import platform 
+import subprocess 
 
 from cloud_linux.secrets import vault
-from cloud_linux.labs.test import test, ask as input
-from cloud_linux.labs.files import randpath, make_flag, random_big_file
+from cloud_linux.lab import test, ask as input, LinuxLab
+from cloud_linux.labs.files import randpath, make_flag, random_big_file, check_files, setup_files
 
 vault.setkey("blarny234")
-#vault.setfile(f'{os.environ["HOME"]}/.bosslab')
+vault.setfile(f'{os.environ["HOME"]}/.midterm1')
 
-debug = False
+debug = True
+test = LinuxLab(debug=debug)
 
 @test.question
 def kernel_version():
@@ -61,7 +60,7 @@ def flag_file():
     if debug:
         print("DEBUG:", f['secret'])
     got = input().strip()
-    assert got == f['secret'], """That's not correct."""
+    assert got == str(f['secret']), """That's not correct."""
 
 
 @test.question
@@ -96,6 +95,28 @@ def file_size(file):
 
 
 @test.question
+def relative_path(dir1, dir2):
+    """
+    What is the relative path between the following two directories?
+
+      from: {dir1}
+        to: {dir2}
+
+    """
+    got = input().strip()
+    assert not got.startswith('cd'), """The answer should not start with "cd"."""
+
+    got = pathlib.Path(got)
+    assert not got.is_absolute(), """That's an absolute path."""
+
+    rel = (dir1 / got).resolve()
+    if debug:
+        print("That makes:", rel)
+
+    assert dir2.resolve() == rel, f"""That's not right.""" 
+
+
+@test.question
 def flag_inode():
     """
     I've (re)created a file in your home directory called "flag". 
@@ -109,7 +130,7 @@ def flag_inode():
         print("DEBUG:", exp)
     got = int(input())
     assert got == exp, """That's not the correct number.""" 
-    
+
 
 @test.question
 def directory_inode(dir):
@@ -129,7 +150,9 @@ def directory_inode(dir):
 @test.question
 def first_word():
     """
-    I just created a file called "bigfile" in the current directory. What is the first word in it? 
+    I just created a file called "~/bigfile" your home directory. 
+    
+    What is the first word in it? 
     """
     bigfile = random_big_file()
     with open(bigfile) as fh:
@@ -156,7 +179,7 @@ def file_type(file):
 
 
 @test.question
-def relative_path2(dir1, dir2):
+def relative_path(dir1, dir2):
     """
     What is the relative path between the following two directories?
 
@@ -208,22 +231,138 @@ def link_type(link):
     assert got == 'absolute' and link.is_absolute() \
         or got == 'relative' and not link.is_absolute(), """That's not correct."""
 
+start_files = [    
+    ['Hawaii',   None, None, 'Island in the Pacific ocean'],
+    ['Samoa',    None, None, 'Island in the Pacific ocean'],
+    ['Kiribati', None, None, 'Island in the Pacific ocean'],
+    ['Ireland',  None, None, 'Island in the Atlantic ocean'],
+    ['Madeira',  None, None, 'Island in the Atlantic ocean'],
+    ['Azores',   None, None, 'Island in the Atlantic ocean'],
+    ['Langkawi', None, None, 'Island in the Indian ocean'],
+    ['Sabang',   None, None, 'Island in the Indian ocean'],
+    ['Nublar',   None, None, 'Island in Fiction'],
+    ['Hydra',    None, None, 'Island in Fiction'],
+]
+
+end_files = [
+    ['Pacific/Hawaii',   None, None, 'Island in the Pacific ocean'],
+    ['Pacific/Samoa',    None, None, 'Island in the Pacific ocean'],
+    ['Pacific/Kiribati', None, None, 'Island in the Pacific ocean'],
+    ['Atlantic/Ireland', None, None, 'Island in the Atlantic ocean'],
+    ['Atlantic/Madeira', None, None, 'Island in the Atlantic ocean'],
+    ['Atlantic/Azores',  None, None, 'Island in the Atlantic ocean'],
+    ['Indian/Langkawi',  None, None, 'Island in the Indian ocean'],
+    ['Indian/Sabang',    None, None, 'Island in the Indian ocean'],
+    ['Fictional/Nublar', None, None, 'Island in Fiction'],
+    ['Fictional/Hydra',  None, None, 'Island in Fiction'],
+]
+
+@test.question
+def test_pacific():
+    """I'm about to check the Pacific islands."""
+    check_files({
+        'files': end_files[0:3], 
+        'basedir': pathlib.Path.home() / 'Oceans'
+        }, extra=True)
+
+
+@test.question
+def test_atlantic():
+    """I'm about to check the Atlantic islands."""
+    check_files({
+        'files': end_files[3:6], 
+        'basedir': pathlib.Path.home() / 'Oceans'
+        }, extra=True)
+
+
+@test.question
+def test_indian():
+    """I'm about to check the Indian islands."""
+    check_files({
+        'files': end_files[6:8], 
+        'basedir': pathlib.Path.home() / 'Oceans'
+        }, extra=True)
+
+
+@test.question
+def test_fictional():
+    """I'm about to check the Fictional islands."""
+    check_files({
+        'files': end_files[8:], 
+        'basedir': pathlib.Path.home() / 'Oceans'
+        }, extra=True)
+
+@test.question
+def test_extras():
+    """I'm about to check for extraneous files."""
+    check_files({
+        'files': end_files, 
+        'basedir': pathlib.Path.home() / 'Oceans'
+        }, extra=False)
+
+
+def update_code():
+    print("Updated code (not needed in the practice):", vault.confirmation({'score': test.score}))
 
 def main():
 
     kernel_version(points=5)
+    update_code()
     total_mem(points=5)
+    update_code()
     distro(points=5)
+    update_code()
     flag_file(points=5)
+    update_code()
     flag_path(points=5)
+    update_code()
     file_size(points=5, file=randpath.random_file())
+    update_code()
     flag_inode(points=5)
+    update_code()
     directory_inode(points=5, dir=randpath.random_dir())
+    update_code()
     first_word(points=5)
+    update_code()
     file_type(points=5, file=randpath.find(lambda x: x.suffix == '.gz' and not x.is_symlink()))
-    relative_path2(points=5, dir1=randpath.random_dir(), dir2=randpath.random_dir())
+    update_code()
+    relative_path(points=5, dir1=randpath.random_dir(), dir2=randpath.random_dir())
+    update_code()
     resolve_link(points=5, link=randpath.find(lambda x: x.is_symlink()))
+    update_code()
     link_type(points=5, link=randpath.find(lambda x: x.is_symlink()))
+    update_code()
+
+    print("""
+    I have created a directory called "Islands" in your home directory. 
+    Inside of "Islands" you will see 10 files named after islands. Each island 
+    file contains the name of the ocean it is in. Reorganize the files so 
+    that they are in directories named after their oceans. The reorganized 
+    files should be in a directory called "Oceans" in the current directory.  
+    
+    The "Oceans" directory should look like this: 
+
+    .
+    |-- Oceans
+        |-- Atlantic
+        |-- Pacific
+        |-- Indian
+        |-- Fictional
+
+    """)
+
+    setup_files({
+        'files': start_files, 
+        'basedir': pathlib.Path.home() / 'Islands'
+    })
+
+    input("Check for the files and press Enter.")
+
+    test_pacific(points=4)
+    test_atlantic(points=4)
+    test_indian(points=4)
+    test_fictional(points=4)
+    test_extras(points=4)
 
     #print(f"Your score is {test.score} of {test.total}")
     #print("Your confirmation code is:", vault.confirmation({'score': test.score}))
