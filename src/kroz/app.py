43123ca18f5b01802f2ece1787d16bae93d8309a
Mike Matera <michael.matera@cabrillo.edu>
2025-03-10 08:59:36 -0700

    Working on workers!

--- src/kroz/app.py
+++ src/kroz/app.py
@@ -1,54 +1,75 @@
 """
-The Kroz UI Module 
+The Kroz UI Module
 """
 
+from enum import Enum
 from textual.app import App
-from textual.worker import Worker
+from textual.worker import Worker, WorkerState, WorkType
+
 
 class KrozApp(App):
     """A UI application."""
 
+    class State(Enum):
+        INIT = 1
+        SETUP = 2
+        MAIN = 3
+        CLEANUP = 4
+        EXIT = 5
+
     def __init__(self, title: str):
         super().__init__()
-        self._title = title 
-        self._setup = None 
-        self._cleanup = None 
-        self._main = None 
+        self._title = title
+        self._setup = None
+        self._cleanup = None
+        self._main = None
+        self._state = KrozApp.State.INIT
 
-    def setup(self, func):
+    def setup(self, func: WorkType):
         """Decorator for the setup() function."""
-        self._setup = func 
+        self._setup = func
         return func
 
-    def cleanup(self, func):
+    def cleanup(self, func: WorkType):
         """Decorator for the cleanup() function."""
-        self._cleanup = func 
+        self._cleanup = func
         return func
 
-    def main(self, func):
+    def main(self, func: WorkType):
         """Decorator for the main() function."""
-        self._main = func 
+        self._main = func
         return func
 
     def _call(self, func):
         """Call async or sync."""
-        if callable(func):
-            self.run_worker(func, thread=True)
-        else:
-            self.run_worker(func)
+        if func is not None:
+            if callable(func):
+                self.run_worker(func, thread=True)
+            else:
+                self.run_worker(func)
 
     def on_mount(self):
-        if self._setup:
+        if self._state == KrozApp.State.INIT:
+            self._state = KrozApp.State.SETUP
             self._call(self._setup)
-        try:
-            if self._main:
-                self._call(self._main)
-        
-        finally:
-            if self._cleanup:
-                self._call(self._cleanup)
+        else:
+            self.log("Mount while not in init.")
 
     def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
         """Called when the worker state changes."""
-        self.log(event)
+        if self._state == self.State.SETUP:
+            if event.state == WorkerState.SUCCESS:
+                self._state = self.State.MAIN
+                self._call(self._main)
+            else:
+                raise
+        elif self._state == self.State.MAIN:
+            pass
+        elif self._state == self.State.CLEANUP:
+            pass
+        elif self._state == self.State.EXIT:
+            pass
+        else:
+            raise ValueError("Unexpected state in state change:", event)
 
+        self.log(event)
