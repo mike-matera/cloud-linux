4b43b67c029d4e665185e101daf804957f47a2c9
Mike Matera <matera@lifealgorithmic.com>
2025-05-26 17:07:25 -0700

    Add week9 stubs.

---
+++ src/kroz/labs/week09.py
@@ -0,0 +1,28 @@
+"""
+Manage files with permissions.
+"""
+
+
+import kroz
+from kroz.questions.week09 import Islands2
+
+WELCOME = """
+# Sort the Islands
+
+This lab builds on the original islands lab. It covers lessons X and Y. To do 
+this lab you will need to understand the `chmod` and `chgrp` commands. 
+
+Good luck!
+"""
+
+app = kroz.KrozApp('Sort the Islands', debug=True)
+
+
+@app.main
+def main():
+    app.show(WELCOME, classes="welcome")
+    app.ask(Islands2())
+
+
+if __name__ == "__main__":
+    quit(app.run())
---
+++ src/kroz/questions/week09.py
@@ -0,0 +1,71 @@
+"""
+Week 9 Questions 
+"""
+
+import grp
+from pathlib import Path
+import textwrap
+from kroz.random.path import CheckDir, CheckFile, CheckPath
+
+from .week05 import Islands
+
+class Islands2(Islands):
+    """The islands with perms."""
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        try:
+            grp.getgrnam("cis90")
+            cis90_grp = "cis90"
+        except KeyError:
+            cis90_grp = "adm"  # for testing on my machine
+
+        self.check_files = CheckPath(
+            "Oceans",
+            files=[
+                CheckDir("", group=cis90_grp, perms=0o777),
+                CheckDir("Pacific", group=cis90_grp, perms=0o660),
+                CheckDir("Atlantic", group=cis90_grp, perms=0o440),
+                CheckDir("Indian", group=cis90_grp, perms=0o600),
+                CheckDir("Fictional", group=cis90_grp, perms=0o600),
+            ],
+        )
+        for file in self.start_files.files:
+            assert isinstance(file, CheckFile), "Internal error."
+            if "Pacific" in file.contents:
+                newpath = Path("Pacific") / file.path
+                newperms = 0o660
+            elif "Atlantic" in file.contents:
+                newpath = Path("Atlantic") / file.path
+                newperms = 0o440
+            elif "Indian" in file.contents:
+                newpath = Path("Indian") / file.path
+                newperms = 0o600
+            elif "fiction" in file.contents:
+                newpath = Path("Fictional") / file.path
+                newperms = 0o600
+            else:
+                raise ValueError("Ooops:", file.contents)
+            self.check_files.files.append(
+                CheckFile(
+                    newpath,
+                    contents=file.contents,
+                    perms=newperms,
+                    group=cis90_grp,
+                )
+            )
+
+    @property
+    def text(self): 
+        return textwrap.dedent("""
+        # Island Permissions 
+        
+        I have created a directory called `Islands` with the island files from 
+        the previous island assignment. For this assignment update the 
+        permissions of your isldand files to match the following:
+                               
+        {}
+        """).format(self.check_files.markdown(detail=True))
+
