9c67922c2f3b1bb8fcd441f8a255eb3feaf05fde
Mike Matera <michael.matera@cabrillo.edu>
2025-07-14 18:12:58 -0700

    Improve reports on startup.

--- src/kroz/bash/setup.sh
+++ src/kroz/bash/setup.sh
@@ -1,14 +1,14 @@
 KROZ_SOCKET="${HOME}/.kroz/command.sock:5000"
 
 function cmd_report {
-    if [ "$BASH_COMMAND" != "prompt_report" ]; then
+    if [[ "$BASH_COMMAND" != "prompt_report" && "$BASH_COMMAND" != "PROMPT_COMMAND=prompt_report" ]]; then
         kroz_saved_command=$(echo "$BASH_COMMAND" | base64)
     fi
 }
 
 function prompt_report {
     local EXIT="$?"
-    if [ -S "$KROZ_SOCKET" ]; then
+    if [[ -S "$KROZ_SOCKET" && ! -z "$kroz_saved_command" ]]; then
         curl -d \{\"cmd\":\"$kroz_saved_command\",\"result\":$EXIT,\"cwd\":\"$(pwd | base64)\"\} -H "Content-Type: application/json" -X POST --unix-socket "$KROZ_SOCKET" http://report > /dev/null 2>&1
     fi
 }
