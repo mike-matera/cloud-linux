0c48584618147ce3e6782f13c2cb68a17713cecf
Mike Matera <michael.matera@cabrillo.edu>
2025-08-31 09:07:20 -0700

    Finish lesson 6.

--- src/kroz/questions/README.md
+++ src/kroz/questions/README.md
@@ -10,17 +10,19 @@ Complete pending a final proof read:
 * Lesson 4
 * Lesson 5
 * Lesson 7
+* Lesson 6
 
 Incomplete:
 
-* Lesson 6 (all)
 * Lesson 8 (Walks, Questions)
 * Lesson 9 (Walks, Questions, Integrate Islands2)
 * Lesson 10 (Walks, Questions, Integrate Processes)
 * Lesson 11 (all)
 * Lesson 12 (all)
 * Lesson 13 (all)
-
+* Midterm 1 
+* Midterm 2 
+* Final Project
 
 ## Requirement:
 
@@ -30,6 +32,7 @@ Incomplete:
     * Total: 13 (ugh)
 * 12 take home labs 
     * Lesson 2 - Lesson 13 
-    * Midterm 1 + Midterm 2 + Final Project
-    * Total: 16
+    * P1 + P2 + Midterm 1 + Midterm 2 + Final Project
+    * Total: 18
 * A test bank of 100 challenges 
+    * Count program questions + Static questions (should be more than enough)
--- src/kroz/questions/lesson06.py
+++ src/kroz/questions/lesson06.py
@@ -13,7 +13,12 @@ Reading:
 from typing import Type
 
 from kroz.flow.base import KrozFlowABC
-from kroz.flow.question import MultipleChoiceQuestion, Question
+from kroz.flow.question import (
+    MultipleChoiceQuestion,
+    Question,
+    ShortAnswerQuestion,
+    TrueOrFalseQuestion,
+)
 from kroz.screen import KrozScreen
 
 title = "Getting Help"
@@ -43,12 +48,147 @@ walks: dict[str, list[KrozFlowABC | Type[KrozFlowABC]]] = {}
 
 questions: list[Question] = [
     MultipleChoiceQuestion(
-        "What command would you use to *rename* a file?",
-        "mv",
-        "cp",
-        "ln -s",
-        "rm",
+        """According to the book what is the most brutal man page of them all?""",
+        "bash",
+        "cat",
+        "mount",
+        "lvcreate",
+    ),
+    TrueOrFalseQuestion(
+        "Manual pages on the web are more relevant than the ones on the system.",
+        False,
+    ),
+    MultipleChoiceQuestion(
+        """
+Here is the help for a command:
+
+```
+mycommand [-t] [-v] name...
+```
+
+Which is of the following is true of this command:
+""",
+        "You must supply one or more `name` arguments",
+        "The `-t` argument is required",
+        "The `-v` argument is required",
+        "The -h shows you command help",
+    ),
+    MultipleChoiceQuestion(
+        """
+Here is the help for a command:
+
+```
+mycommand {-t|-v} [name...]
+```
+
+Which is of the following is true of this command:
+""",
+        "One of `-t` or `-v` is required (but not both)",
+        "You must supply one or more `name` arguments",
+        "The `-t` argument is required",
+        "The `-v` argument is required",
+    ),
+    MultipleChoiceQuestion(
+        """
+Here is the help for a command:
+
+```
+mycommand [-t|-v] [name]
+```
+
+Which is of the following is true of this command:
+""",
+        "It can be executed with no arguments"
+        "You must supply one or more `name` arguments",
+        "The `-t` argument is required",
+        "The `-v` argument is required",
+    ),
+    MultipleChoiceQuestion(
+        """
+Here is the help for a command:
+
+```
+mycommand [-t|-v] name
+mycommand -x name
+```
+
+Which is of the following is true of this command:
+""",
+        "It has two forms, one with `-x` and one without"
+        "The `name` argument is optional",
+        "The `-x` argument is required",
+        "The `-t` argument is required",
     ),
 ]
 
-lab: dict[str, list[Question]] = {}
+lab: dict[str, list[Question]] = {
+    "Enable X11 forwarding in SSH": [
+        ShortAnswerQuestion(
+            """What option in SSH enables X11 forwarding?""",
+            "-X",
+            help="Capitalization matters!",
+        ),
+    ],
+    "Debugging in SSH": [
+        ShortAnswerQuestion(
+            """What option in SSH the maximum level of debugging?""",
+            "-vvv",
+            help="""
+# Repeated Flags 
+
+Sometimes repeating a flag give you *more* of that flag. For example if a 
+command accepts `-a` it may also accept `-aa` to do the more of the same thing.
+Look in the SSH manual for the flag that enables debugging and repeat it the 
+maximum number of times. 
+""",
+        ),
+    ],
+    "Use an alternate key file.": [
+        ShortAnswerQuestion(
+            """
+What arguments would you give to SSH to tell it to use `~/my_key` as an
+*identity file* (a.k.a. private key). 
+            """,
+            "-i ~/my_key",
+            help="""
+# A Flag With Options 
+
+Flags can have options. For example the SSH has the `-l` option where you can 
+specify your username on the remote system. You would use it like this:
+
+```console
+$ ssh -l mich431 opus.cis.cabrillo.edu
+```
+
+When `-l` is on the command line the next argument will be interpreted as the 
+name of a user. It's an error to omit the argument to `-l`. 
+""",
+        ),
+    ],
+    "Use SSH to run a command remotely.": [
+        ShortAnswerQuestion(
+            """
+# SSH is Awesome 
+
+Sometimes you just want to run a single command or script on a remote system 
+instead of logging in. SSH lets you do that by placing a UNIX command as the 
+last thing on the SSH command line. For example, You can just run `ls` on opus
+from your home computer like this:
+
+```console
+MyComputer> ssh opus.cis.cabrillo.edu ls 
+```
+
+What command, when run from your home computer, runs the `top` program on opus?
+""",
+            "ssh -t opus.cis.cabrillo.edu top",
+            help="""
+# What's Missing? 
+
+Run the `top` program in an interactive login and you can see that it's an 
+interactive program. SSH needs an extra argument to run interactive programs 
+directly. **Google it**. 
+""",
+        )
+    ],
+}
