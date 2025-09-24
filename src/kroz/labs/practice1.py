bf5169af3f2b6a59483abf397f56d06894e416cb
Mike Matera <michael.matera@cabrillo.edu>
2025-09-24 10:36:51 -0700

    Refactor practice.

--- src/kroz/labs/practice1.py
+++ src/kroz/labs/practice1.py
@@ -4,36 +4,32 @@ The practice midterm.
 
 import datetime
 
+from kroz.app import KrozApp
 from kroz.flow.base import FlowContext
 from kroz.questions.lesson02 import FreeMemory, NewYearFuture, OsRelease
 from kroz.questions.lesson03 import PathAttrs, RelativePaths
 from kroz.questions.lesson04 import FileType, LinkInfo, MakeLink, WordInBigfile
 
+app = KrozApp("Practice Midterm 1")
 
+
+@app.main
 def run():
-    from kroz.app import KrozApp
-
-    app = KrozApp("Practice Midterm 1")
-
-    def run():
-        if "started" not in app.state:
-            app.state["started"] = datetime.datetime.now()
-        try:
-            with FlowContext("questions", progress=True, points=10) as flow:
-                flow.run(NewYearFuture(tries=1))
-                flow.run(OsRelease("VERSION_CODENAME"))
-                flow.run(FreeMemory(key="used"))
-                flow.run(FileType())
-                flow.run(RelativePaths())
-                flow.run(WordInBigfile(cols=5, find=(2, 2)))
-                flow.run(PathAttrs(type=PathAttrs.AttrType.PERMS))
-                flow.run(LinkInfo(type=LinkInfo.Info.TARGET))
-                flow.run(MakeLink(name="practice1", rel=True))
-        finally:
-            app.state["exited"] = datetime.datetime.now()
-
-    app.main(run)
-    app.run()
+    if "started" not in app.state:
+        app.state["started"] = datetime.datetime.now()
+    try:
+        with FlowContext("questions", progress=True, points=10) as flow:
+            flow.run(NewYearFuture(tries=1))
+            flow.run(OsRelease("VERSION_CODENAME"))
+            flow.run(FreeMemory(key="used"))
+            flow.run(FileType())
+            flow.run(RelativePaths())
+            flow.run(WordInBigfile(cols=5, find=(2, 2)))
+            flow.run(PathAttrs(type=PathAttrs.AttrType.PERMS))
+            flow.run(LinkInfo(type=LinkInfo.Info.TARGET))
+            flow.run(MakeLink(name="practice1", rel=True))
+    finally:
+        app.state["exited"] = datetime.datetime.now()
 
 
 def main():
@@ -43,7 +39,7 @@ def main():
         print("Sorry.")
         return
 
-    run()
+    app.run()
 
 
 if __name__ == "__main__":
