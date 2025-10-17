a632e0ba1f7f6dd614642506805a57d667f8f240
Mike Matera <michael.matera@cabrillo.edu>
2025-10-16 17:36:45 -0700

    Fix for better bash compatibility.

--- src/kroz/bash/setup.sh
+++ src/kroz/bash/setup.sh
@@ -3,7 +3,7 @@ KROZ_SOCKET="${HOME}/.kroz/command.sock:5000"
 function cmd_report {
     if [[ -S "$KROZ_SOCKET" && "$BASH_COMMAND" != "prompt_report" && "$BASH_COMMAND" != "PROMPT_COMMAND=prompt_report" ]]; then
         message=\{\"cmd\":\"$(echo "$BASH_COMMAND" | base64)\"\}
-        if [[ ! -z "$KROZ_REPORT_DEBUG" ]] then 
+        if [[ ! -z "$KROZ_REPORT_DEBUG" ]]; then 
             echo $message
         fi
         curl -d $message -H "Content-Type: application/json" -X POST --unix-socket "$KROZ_SOCKET" http://report > /dev/null 2>&1
@@ -14,7 +14,7 @@ function prompt_report {
     local EXIT="$?"
     if [[ -S "$KROZ_SOCKET" ]]; then
         message=\{\"result\":$EXIT,\"cwd\":\"$(pwd | base64)\"\}
-        if [[ ! -z "$KROZ_REPORT_DEBUG" ]] then 
+        if [[ ! -z "$KROZ_REPORT_DEBUG" ]]; then 
             echo $message
         fi
         curl -d $message -H "Content-Type: application/json" -X POST --unix-socket "$KROZ_SOCKET" http://report > /dev/null 2>&1
