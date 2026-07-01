2f00081f7980ac9549f72b26872c1f550a7cbebc
Mike Matera <matera@lifealgorithmic.com>
2026-06-30 23:54:17 -0700

    Exam skips questions as intended.

--- src/kroz/labs/exam.py
+++ src/kroz/labs/exam.py
@@ -3,9 +3,18 @@ Helper for doing an exam
 """
 
 import datetime
+from dataclasses import dataclass
 
 from kroz.app import KrozApp
 from kroz.flow.base import FlowResult
+
+
+@dataclass
+class StateItem:
+    number: int
+    status: FlowResult
+    score: float
+    updated: datetime.datetime = datetime.datetime.now(datetime.UTC)
 
 
 def exam(*questions, timelimit=0):
@@ -14,22 +23,33 @@ def exam(*questions, timelimit=0):
     if "started" not in app.state:
         app.state["started"] = now
 
+    # Build initial status
+    state: list[StateItem] = [
+        StateItem(number=i, status=FlowResult.INCOMPLETE, score=0)
+        for i in range(len(questions))
+    ]
+    state = app.state.get("status", default=state, store=True)
+
+    app.set_score(sum([s.score for s in state]))
+
     if timelimit > 0:
         if now > app.state["started"] + datetime.timedelta(minutes=timelimit):
             return "You have no more time to do this test."
 
     try:
-        for question in questions:
-            result = question.show()
-            if result == FlowResult.CORRECT:
-                if question.points is not None:
-                    app.update_score(question.points)
+        for number, question in enumerate(questions):
+            if state[number].status != FlowResult.CORRECT:
+                state[number].status = question.show()
+                if state[number].status == FlowResult.CORRECT:
+                    if question.points is not None:
+                        state[number].score = question.points
+                        app.update_score(question.points)
 
-            if timelimit > 0:
-                if datetime.datetime.now(datetime.UTC) > app.state[
-                    "started"
-                ] + datetime.timedelta(minutes=timelimit):
-                    return "You are out of time."
+                if timelimit > 0:
+                    if datetime.datetime.now(datetime.UTC) > app.state[
+                        "started"
+                    ] + datetime.timedelta(minutes=timelimit):
+                        return "You are out of time."
 
     finally:
         app.state["exited"] = datetime.datetime.now(datetime.UTC)
